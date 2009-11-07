78269905c01eb3f9e478358197a8d1416cf195e8
John Pignata <john.pignata@gmail.com>
2009-11-07 14:25:19 -0500

    Bash updates

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 
 for item in $(find dot); do
 	target=~/`echo $item | sed -e 's@dot/*@.@'`
-	
+
 	if [ -d $item ]; then
-		test -d $target || mkdir -p $target
+  	test -d $target || mkdir -p $target
 	elif [ -f $item ]; then
-		echo "Copying ${item} to ${target}" 
-		cp $item $target
+	echo "Copying ${item} to ${target}" 
+  	cp $item $target
 	fi
 done
