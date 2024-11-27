e664cac3b8b6aaee3f5f3829e8d9d2006cd2aa53
John Pignata <john@pignata.com>
2024-11-26 19:38:36 -0500

    dotfiles makeover

---
+++ home/dot/config/nvim/init.lua
@@ -0,0 +1,49 @@
+require("config.lazy")
+
+require'nvim-treesitter.configs'.setup {
+    ensure_installed = "all",
+    sync_install = false,
+    auto_install = true,
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
+}
+
+require("nvim-tree").setup()
+
+require('lualine').setup {
+    options = {
+        disabled_filetypes = {
+            winbar = { 'NvimTree' },
+            statusline = { 'NvimTree' },
+        },
+        refresh = {
+            statusline = 200,
+            tabline = 200,
+            winbar = 200,
+        }
+    }
+}
+
+vim.keymap.set("n", "<leader>n", ":NvimTreeToggle<CR>", { desc = "Toggle NvimTree" })
+
+vim.opt.termguicolors = true
+
+vim.g.mapleader = " "
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.o.autoindent = true
+vim.o.clipboard = "unnamedplus"
+vim.o.expandtab = true
+vim.o.foldcolumn = '1'
+vim.o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+vim.o.foldlevel = 99
+vim.o.foldmethod = 'expr'
+vim.o.number = true
+vim.o.shiftwidth = 4
+vim.o.smartindent = true
+vim.o.tabstop = 4
+
+vim.cmd("colorscheme nightfox")
